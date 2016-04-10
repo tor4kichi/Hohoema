bb83f2e10111953f054e7ee8e849b190012d66c3
torakichi <tor4kichi@gmail.com>
2016-04-10 16:57:23 +0900

    unity + prismによるアプリ起動をセットアップ

---
+++ NicoPlayerHohoema/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Windows.Mvvm;
+using Prism.Windows.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoPlayerHohoema.ViewModels
+{
+	public class MainPageViewModel : ViewModelBase
+	{
+		private string message = "Hello world";
+
+		public string Message
+		{
+			get { return this.message; }
+			set { this.SetProperty(ref this.message, value); }
+		}
+
+		public override void OnNavigatedTo(NavigatedToEventArgs e, Dictionary<string, object> viewModelState)
+		{
+			base.OnNavigatedTo(e, viewModelState);
+			Debug.WriteLine("MainPageにきた");
+		}
+
+		public override void OnNavigatingFrom(NavigatingFromEventArgs e, Dictionary<string, object> viewModelState, bool suspending)
+		{
+			base.OnNavigatingFrom(e, viewModelState, suspending);
+			Debug.WriteLine("MainPageから去る");
+		}
+	}
+}
--- NicoPlayerHohoema/Views/MainPage.xaml.cs
+++ NicoPlayerHohoema/Views/MainPage.xaml.cs
@@ -15,13 +15,13 @@ using Windows.UI.Xaml.Navigation;
 
 // 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
 
-namespace NicoPlayerHohoema
+namespace NicoPlayerHohoema.Views
 {
     /// <summary>
     /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
     /// </summary>
     public sealed partial class MainPage : Page
-    {
+	{
         public MainPage()
         {
             this.InitializeComponent();
