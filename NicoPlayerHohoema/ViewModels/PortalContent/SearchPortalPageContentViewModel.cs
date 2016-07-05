30e646075d077355de93680919f08a69ea694cf7
torakichi <tor4kichi@gmail.com>
2016-07-05 17:43:02 +0900

    #27 ポータルページに検索履歴の表示を追加

--- NicoPlayerHohoema/ViewModels/PortalContent/SearchPortalPageContentViewModel.cs
+++ NicoPlayerHohoema/ViewModels/PortalContent/SearchPortalPageContentViewModel.cs
@@ -2,9 +2,11 @@
 using NicoPlayerHohoema.Views.Service;
 using Prism.Commands;
 using Prism.Windows.Mvvm;
+using Reactive.Bindings;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,13 +14,18 @@ namespace NicoPlayerHohoema.ViewModels.PortalContent
 {
 	public class SearchPortalPageContentViewModel : PotalPageContentViewModel
 	{
-		// TODO: 検索履歴の表示
-
 		public SearchPortalPageContentViewModel(PageManager pageManager, HohoemaApp hohoemaApp, ISearchDialogService searchDialog)
 			: base(pageManager)
 		{
 			_HohoemaApp = hohoemaApp;
 			_SearchDialog = searchDialog;
+
+			var searchSettings = _HohoemaApp.UserSettings.SearchSettings;
+
+			HistoryKeywords = searchSettings.SearchHistory
+				.ToReadOnlyReactiveCollection(x => 
+					new PortalSearchHisotryItem(x, PageManager)
+				);
 		}
 
 
@@ -35,9 +42,45 @@ namespace NicoPlayerHohoema.ViewModels.PortalContent
 			}
 		}
 
+		public ReadOnlyReactiveCollection<PortalSearchHisotryItem> HistoryKeywords { get; private set; }
+
 
 
 		HohoemaApp _HohoemaApp;
 		ISearchDialogService _SearchDialog;
+	}
+
+	public class PortalSearchHisotryItem
+	{
+		public PortalSearchHisotryItem(string keyword, PageManager pageManager)
+		{
+			_PageManager = pageManager;
+			Keyword = keyword;
+		}
+
+
+		private DelegateCommand _SelectedKeywordCommand;
+		public DelegateCommand SelectedKeywordCommand
+		{
+			get
+			{
+				return _SelectedKeywordCommand
+					?? (_SelectedKeywordCommand = new DelegateCommand(() =>
+					{
+						_PageManager.OpenPage(HohoemaPageType.Search,
+							new SearchOption()
+							{
+								Keyword = Keyword,
+								SearchTarget = SearchTarget.Keyword,
+								SortMethod = Mntone.Nico2.SortMethod.FirstRetrieve,
+								SortDirection = Mntone.Nico2.SortDirection.Descending,
+							}.ToParameterString());
+					}));
+			}
+		}
+
+		private PageManager _PageManager;
+		public string Keyword { get; private set; }
+
 	}
 }
