c4be591f5bd767fe9146529fa6dac913441be9ea
torakichi <tor4kichi@gmail.com>
2016-07-14 09:50:17 +0900

    #91 リリースビルドかつタブレットで一時停止と再生ボタンが機能しなかった問題に対応

---
+++ NicoPlayerHohoema/Views/Behaviors/MediaElementPauseAction.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xaml.Interactivity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace NicoPlayerHohoema.Views.Behaviors
+{
+	public class MediaElementPauseAction : Behavior<DependencyObject>, IAction
+	{
+		public static readonly DependencyProperty TargetObjectProperty =
+			DependencyProperty.Register("TargetObject"
+					, typeof(MediaElement)
+					, typeof(MediaElementPauseAction)
+					, new PropertyMetadata(null)
+				);
+
+		public MediaElement TargetObject
+		{
+			get { return (MediaElement)GetValue(TargetObjectProperty); }
+			set { SetValue(TargetObjectProperty, value); }
+		}
+
+
+		public object Execute(object sender, object parameter)
+		{
+			var mediaElem = TargetObject;
+			if (mediaElem != null)
+			{
+				mediaElem.Pause();
+			}
+			return true;
+		}
+	}
+}
