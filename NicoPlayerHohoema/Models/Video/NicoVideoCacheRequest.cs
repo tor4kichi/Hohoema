9656d9fc77c94f27e1e746e077700e5dd7a21bd3
torakichi <tor4kichi@gmail.com>
2016-07-19 17:48:27 +0900

    #111 ファイル分割

---
+++ NicoPlayerHohoema/Models/Video/NicoVideoCacheRequest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NicoPlayerHohoema.Models
+{
+	public class NicoVideoCacheRequest : IEquatable<NicoVideoCacheRequest>
+	{
+		public string RawVideoid { get; set; }
+		public NicoVideoQuality Quality { get; set; }
+
+		public override int GetHashCode()
+		{
+			return RawVideoid.GetHashCode() ^ Quality.GetHashCode();
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj is NicoVideoCacheRequest)
+			{
+				return Equals(obj as NicoVideoCacheRequest);
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+		public bool Equals(NicoVideoCacheRequest other)
+		{
+			return this.RawVideoid == other.RawVideoid && this.Quality == other.Quality;
+		}
+	}
+}
---
+++ NicoPlayerHohoema/Models/Video/NiconicoMediaManager.cs
@@ -0,0 +1,111 @@
+﻿using Mntone.Nico2.Videos.Thumbnail;
+using Mntone.Nico2.Videos.WatchAPI;
+using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Storage;
+
+namespace NicoPlayerHohoema.Models
+{
+	/// <summary>
+	/// ニコニコ動画の動画やサムネイル画像、
+	/// 動画情報など動画に関わるメディアを管理します
+	/// </summary>
+	public class NiconicoMediaManager : BindableBase, IDisposable
+	{
+		static internal async Task<NiconicoMediaManager> Create(HohoemaApp app)
+		{
+			var man = new NiconicoMediaManager(app);
+			man.Context = await NicoVideoDownloadContext.Create(app);
+			
+			// ダウンロードリクエストされたアイテムのNicoVideoオブジェクトの作成
+			// 及び、リクエストの再構築
+			var list = await man.Context.LoadDownloadRequestItems().ConfigureAwait(false);
+			foreach (var req in list)
+			{
+				var nicoVideo = await man.GetNicoVideo(req.RawVideoid);
+				await nicoVideo.RequestCache(req.Quality);
+			}
+
+			Debug.WriteLine($"{list.Count}件のダウンロード待ち状況を復元しました。");
+
+			// キャッシュ済みアイテムのNicoVideoオブジェクトの作成
+			var saveFolder = man.Context.VideoSaveFolder;
+			var files = await saveFolder.GetFilesAsync();
+			files
+				.AsParallel()
+				.Where(x => x.Name.EndsWith("_info.json"))
+				.Select(x => new String(x.Name.TakeWhile(y => y != '_').ToArray()))
+				.ForAll(x => man.GetNicoVideo(x).ConfigureAwait(false));
+
+			return man;
+		}
+
+		
+
+		private NiconicoMediaManager(HohoemaApp app)
+		{
+			_HohoemaApp = app;
+
+			VideoIdToNicoVideo = new Dictionary<string, NicoVideo>();
+
+			_NicoVideoSemaphore = new SemaphoreSlim(1, 1);
+
+		}
+		
+
+		
+
+		public async Task<NicoVideo> GetNicoVideo(string rawVideoId)
+		{
+			try
+			{
+				await _NicoVideoSemaphore.WaitAsync().ConfigureAwait(false);
+
+				if (VideoIdToNicoVideo.ContainsKey(rawVideoId))
+				{
+					return VideoIdToNicoVideo[rawVideoId];
+				}
+				else
+				{
+					var nicoVideo = await NicoVideo.Create(_HohoemaApp, rawVideoId, Context);
+					VideoIdToNicoVideo.Add(rawVideoId, nicoVideo);
+					return nicoVideo;
+				}
+			}
+			finally
+			{
+				_NicoVideoSemaphore.Release();
+			}
+		}
+
+
+
+		
+
+
+
+		public void Dispose()
+		{
+			Context.Dispose();
+		}
+
+
+		private SemaphoreSlim _NicoVideoSemaphore;
+
+		public Dictionary<string, NicoVideo> VideoIdToNicoVideo { get; private set; }
+
+		public NicoVideoDownloadContext Context { get; private set; }
+		HohoemaApp _HohoemaApp;
+	}
+
+}
