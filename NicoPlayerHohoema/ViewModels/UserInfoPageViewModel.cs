0565ae60995237bc24ebfa2afcdda5cd8970146e
torakichi <tor4kichi@gmail.com>
2016-08-02 07:11:06 +0900

    #82 ユーザー情報画面でログインユーザーのマイリストデータ取得元がUserMylistManagerになるように修正

--- NicoPlayerHohoema/ViewModels/UserInfoPageViewModel.cs
+++ NicoPlayerHohoema/ViewModels/UserInfoPageViewModel.cs
@@ -197,23 +197,35 @@ namespace NicoPlayerHohoema.ViewModels
 
 			HasOwnerVideo = VideoInfoItems.Count != 0;
 
-			try
-			{
-				await Task.Delay(500);
 
-				var mylistGroups = await HohoemaApp.ContentFinder.GetUserMylistGroups(UserId);
-				foreach (var item in mylistGroups)
+			if (HohoemaApp.LoginUserId.ToString() == UserId)
+			{
+				foreach (var item in HohoemaApp.UserMylistManager.UserMylists)
 				{
 					MylistGroups.Add(new MylistGroupListItem(item, PageManager));
 				}
 			}
-			catch (Exception ex)
+			else
 			{
-				IsLoadFailed = true;
-				Debug.WriteLine(ex.Message);
+				try
+				{
+					await Task.Delay(500);
+
+					var mylistGroups = await HohoemaApp.ContentFinder.GetUserMylistGroups(UserId);
+					foreach (var item in mylistGroups)
+					{
+						MylistGroups.Add(new MylistGroupListItem(item, PageManager));
+					}
+				}
+				catch (Exception ex)
+				{
+					IsLoadFailed = true;
+					Debug.WriteLine(ex.Message);
+				}
+
 			}
 
-			
+
 
 
 			NowLoading = false;
