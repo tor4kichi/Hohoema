f798bca33670e7daa98a2ba5a3343ecfaf531a69
tor4kichi <tor4kichi@hotmail.com>
2016-11-05 17:23:53 +0900

    #204 ステータスバーとナビゲーションバーの領域をアプリが使わないように指定

--- NicoPlayerHohoema/App.xaml.cs
+++ NicoPlayerHohoema/App.xaml.cs
@@ -200,9 +200,11 @@ namespace NicoPlayerHohoema
 			return Task.CompletedTask;
 		}
 
-
 		protected override async Task OnActivateApplicationAsync(IActivatedEventArgs args)
 		{
+			// モバイルで利用している場合に、ナビゲーションバーなどがページに被さらないように指定
+			ApplicationView.GetForCurrentView().SetDesiredBoundsMode(ApplicationViewBoundsMode.UseVisible);
+
 			var pageManager = Container.Resolve<PageManager>();
 
 			if (args.Kind == ActivationKind.ToastNotification)
