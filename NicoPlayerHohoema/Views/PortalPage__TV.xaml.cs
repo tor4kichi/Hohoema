2cd6cdc698a63666d62c539ed207f4ea1e0131e4
tor4kichi <tor4kichi@hotmail.com>
2017-03-07 20:05:31 +0900

    TV向けに用意していたホーム画面を一旦取り下げて、PCと同じ内容を表示

--- NicoPlayerHohoema/Views/PortalPage__TV.xaml.cs
+++ NicoPlayerHohoema/Views/PortalPage__TV.xaml.cs
@@ -20,9 +20,9 @@ namespace NicoPlayerHohoema.Views
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class PortalPage_TV : Page
+    public sealed partial class PortalPage__TV : Page
     {
-        public PortalPage_TV()
+        public PortalPage__TV()
         {
             this.InitializeComponent();
         }
