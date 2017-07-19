0e96c02dc9aca67b38f4ecf5cd4969657c210a02
tor4kichi <tor4kichi@hotmail.com>
2017-07-19 14:41:54 +0900

    #479 ページ遷移時にメニューがパカパカする問題に対応

--- NicoPlayerHohoema/Views/MenuNavigatePageBase.xaml.cs
+++ NicoPlayerHohoema/Views/MenuNavigatePageBase.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -25,15 +26,68 @@ namespace NicoPlayerHohoema.Views
 		{
 			this.InitializeComponent();
 
-			this.Loading += MenuNavigatePageBase_Loading;
+            this.Loading += MenuNavigatePageBase_Loading;
+            this.Loaded += MenuNavigatePageBase_Loaded;
+
 		}
 
-		private void MenuNavigatePageBase_Loading(FrameworkElement sender, object args)
+
+        private void MenuNavigatePageBase_Loading(FrameworkElement sender, object args)
 		{
 			ForceChangeChildDataContext();
+
         }
 
-		private void ForceChangeChildDataContext()
+        private void MenuNavigatePageBase_Loaded(object sender, RoutedEventArgs e)
+        {
+            UINavigationManager.Pressed += UINavigationManager_Pressed;
+
+            var pane = GetTemplateChild("PaneLayout") as FrameworkElement;
+
+            pane.GotFocus += RootLayout_GotFocus;
+            pane.LostFocus += RootLayout_LostFocus;
+        }
+
+        private bool _IsFocusing = false;
+
+        private int LeftInputCount = 0;
+
+        private void RootLayout_GotFocus(object sender, RoutedEventArgs e)
+        {
+            _IsFocusing = true;
+        }
+
+        private void RootLayout_LostFocus(object sender, RoutedEventArgs e)
+        {
+            _IsFocusing = false;
+        }
+
+
+
+        private void UINavigationManager_Pressed(UINavigationManager sender, UINavigationButtons buttons)
+        {            
+            var splitView = GetTemplateChild("ContentSplitView") as SplitView;
+            if (_IsFocusing && buttons == UINavigationButtons.Left)
+            {
+                LeftInputCount++;
+                if (LeftInputCount > 1)
+                {
+                    splitView.IsPaneOpen = true;
+                }
+            }
+            else
+            {
+                LeftInputCount = 0;
+
+                if (buttons == UINavigationButtons.Accept || buttons == UINavigationButtons.Right)
+                {
+                    splitView.IsPaneOpen = false;
+                }
+            }
+            
+        }
+
+        private void ForceChangeChildDataContext()
 		{
 			if (Parent is FrameworkElement && Content is FrameworkElement)
 			{
