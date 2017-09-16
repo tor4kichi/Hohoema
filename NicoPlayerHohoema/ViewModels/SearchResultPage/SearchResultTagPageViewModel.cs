e40c0cde86b03124c09d50e74515a9f34ddf7c8d
tor4kichi <tor4kichi@hotmail.com>
2017-09-16 16:35:11 +0900

    #401 タグ検索結果ページでの検索オプション変更に対応

--- NicoPlayerHohoema/ViewModels/SearchResultPage/SearchResultTagPageViewModel.cs
+++ NicoPlayerHohoema/ViewModels/SearchResultPage/SearchResultTagPageViewModel.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -19,7 +20,110 @@ namespace NicoPlayerHohoema.ViewModels
 {
 	public class SearchResultTagPageViewModel : HohoemaVideoListingPageViewModelBase<VideoInfoControlViewModel>
 	{
-		public ReactiveProperty<bool> IsFavoriteTag { get; private set; }
+        private static List<SearchSortOptionListItem> _VideoSearchOptionListItems = new List<SearchSortOptionListItem>()
+        {
+            new SearchSortOptionListItem()
+            {
+                Label = "投稿が新しい順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.FirstRetrieve,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "投稿が古い順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.FirstRetrieve,
+            },
+
+            new SearchSortOptionListItem()
+            {
+                Label = "コメントが新しい順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.NewComment,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "コメントが古い順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.NewComment,
+            },
+
+            new SearchSortOptionListItem()
+            {
+                Label = "再生数が多い順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.ViewCount,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "再生数が少ない順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.ViewCount,
+            },
+
+            new SearchSortOptionListItem()
+            {
+                Label = "コメント数が多い順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.CommentCount,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "コメント数が少ない順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.CommentCount,
+            },
+
+
+            new SearchSortOptionListItem()
+            {
+                Label = "再生時間が長い順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.Length,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "再生時間が短い順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.Length,
+            },
+
+            new SearchSortOptionListItem()
+            {
+                Label = "マイリスト数が多い順",
+                Order = Mntone.Nico2.Order.Descending,
+                Sort = Sort.MylistCount,
+            },
+            new SearchSortOptionListItem()
+            {
+                Label = "マイリスト数が少ない順",
+                Order = Mntone.Nico2.Order.Ascending,
+                Sort = Sort.MylistCount,
+            },
+			// V1APIだとサポートしてない
+			/* 
+			new SearchSortOptionListItem()
+			{
+				Label = "人気の高い順",
+				Sort = Sort.Popurarity,
+				Order = Mntone.Nico2.Order.Descending,
+			},
+			*/
+		};
+
+        public IReadOnlyList<SearchSortOptionListItem> VideoSearchOptionListItems => _VideoSearchOptionListItems;
+
+        public ReactiveProperty<SearchSortOptionListItem> SelectedSearchSort { get; private set; }
+
+        private string _SearchOptionText;
+        public string SearchOptionText
+        {
+            get { return _SearchOptionText; }
+            set { SetProperty(ref _SearchOptionText, value); }
+        }
+
+
+        public ReactiveProperty<bool> IsFavoriteTag { get; private set; }
 		public ReactiveProperty<bool> CanChangeFavoriteTagState { get; private set; }
 
 
@@ -108,7 +212,29 @@ namespace NicoPlayerHohoema.ViewModels
 			})
 			.AddTo(_CompositeDisposable);
 
-		}
+            SelectedSearchSort = new ReactiveProperty<SearchSortOptionListItem>(
+                VideoSearchOptionListItems.First(),
+                mode:ReactivePropertyMode.DistinctUntilChanged
+                );
+
+            SelectedSearchSort
+                .Subscribe(_ =>
+                {
+                    var selected = SelectedSearchSort.Value;
+                    if (SearchOption.Order == selected.Order
+                        && SearchOption.Sort == selected.Sort
+                    )
+                    {
+                        return;
+                    }
+
+                    SearchOption.Sort = SelectedSearchSort.Value.Sort;
+                    SearchOption.Order = SelectedSearchSort.Value.Order;
+
+                    pageManager.Search(SearchOption, forgetLastSearch:true);
+                })
+                .AddTo(_CompositeDisposable);
+        }
 
 		#region Commands
 
@@ -177,9 +303,10 @@ namespace NicoPlayerHohoema.ViewModels
 
             var target = "タグ";
 			var optionText = Util.SortHelper.ToCulturizedText(SearchOption.Sort, SearchOption.Order);
-			UpdateTitle($"{SearchOption.Keyword} - {target}/{optionText}");
+            UpdateTitle($"\"{SearchOption.Keyword}\"");
+            SearchOptionText = $"{target} - {optionText}";
 
-			base.OnNavigatedTo(e, viewModelState);
+            base.OnNavigatedTo(e, viewModelState);
 		}
 
 
