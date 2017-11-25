730c8fad5a3700b6bc384f37805ccb915eeb58c0
tor4kichi <tor4kichi@hotmail.com>
2017-11-25 10:43:00 +0900

    ニコレポの追加読み込み数を増やして追加読み込みの中断を回避するよう修正

--- NicoPlayerHohoema/ViewModels/NicoRepoPageViewModel.cs
+++ NicoPlayerHohoema/ViewModels/NicoRepoPageViewModel.cs
@@ -223,6 +223,11 @@ namespace NicoPlayerHohoema.ViewModels
 
         HohoemaApp HohoemaApp { get; }
 
+        // 通常10だが、ニコレポの表示フィルタを掛けた場合に
+        // 追加読み込み時に表示対象が見つからない場合
+        // 追加読み込みが途絶えるため、多めに設定している
+        public override uint OneTimeLoadCount => 20; 
+
         public LoginUserNicoRepoTimelineSource(HohoemaApp hohoemaApp, IEnumerable<NicoRepoItemTopic> allowedNicoRepoTypes)
         {
             HohoemaApp = hohoemaApp;
