52fd0a0b51a83b627ebb46ba20ed64d4f54762a0
tor4kichi <tor4kichi@hotmail.com>
2018-02-28 10:56:29 +0900

    コミュニティ情報ページを修正

--- NicoPlayerHohoema/Views/CommunityPage.xaml.cs
+++ NicoPlayerHohoema/Views/CommunityPage.xaml.cs
@@ -13,15 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
 namespace NicoPlayerHohoema.Views
 {
-	public sealed partial class CommunityPage : Page
-	{
-		public CommunityPage()
-		{
-			this.InitializeComponent();
-		}
-	}
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class CommunityPage : Page
+    {
+        public CommunityPage()
+        {
+            this.InitializeComponent();
+        }
+    }
 }
