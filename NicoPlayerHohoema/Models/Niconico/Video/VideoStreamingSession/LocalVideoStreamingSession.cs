e56c548fe64bf973abcbcfb93a1c8e48822640ac
tor4kichi <tor4kichi@hotmail.com>
2019-03-07 19:35:15 +0900

    #778 チャンネル動画の暗号化HLSによる再生に対応

--- NicoPlayerHohoema/Models/Niconico/Video/VideoStreamingSession/LocalVideoStreamingSession.cs
+++ NicoPlayerHohoema/Models/Niconico/Video/VideoStreamingSession/LocalVideoStreamingSession.cs
@@ -1,7 +1,12 @@
-﻿using Mntone.Nico2;
+﻿using FFmpegInterop;
+using Mntone.Nico2;
+using Mntone.Nico2.Videos.Thumbnail;
+using NicoPlayerHohoema.Models.Helpers;
 using System;
 using System.Threading.Tasks;
+using Windows.Media.Core;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace NicoPlayerHohoema.Models
 {
@@ -11,10 +16,11 @@ namespace NicoPlayerHohoema.Models
         // MediaSourceをMediaPlayerに設定する役割
 
 
-
         public override NicoVideoQuality Quality { get; }
 
         public StorageFile File { get; }
+
+        FFmpegInteropMSS _VideoMSS;
 
         public LocalVideoStreamingSession(StorageFile file, NicoVideoQuality requestQuality, NiconicoSession niconicoSession)
             : base(niconicoSession)
@@ -23,9 +29,60 @@ namespace NicoPlayerHohoema.Models
             Quality = requestQuality;
         }
 
-        protected override Task<Uri> GetVideoContentUri()
+        protected override async Task<MediaSource> GetPlyaingVideoMediaSource()
         {
-            return Task.FromResult(new Uri(File.Path));
+            var videoUri = new Uri(File.Path);
+
+            MovieType videoContentType = MovieType.Mp4;
+            var tempStream = await HttpSequencialAccessStream.CreateAsync(
+                NiconicoSession.Context.HttpClient
+                , videoUri
+                );
+            if (tempStream is IRandomAccessStreamWithContentType)
+            {
+                var contentType = (tempStream as IRandomAccessStreamWithContentType).ContentType;
+
+                if (contentType.EndsWith("mp4"))
+                {
+                    videoContentType = MovieType.Mp4;
+                }
+                else if (contentType.EndsWith("flv"))
+                {
+                    videoContentType = MovieType.Flv;
+                }
+                else if (contentType.EndsWith("swf"))
+                {
+                    videoContentType = MovieType.Swf;
+                }
+                else
+                {
+                    throw new NotSupportedException($"{contentType} is not supported video format.");
+                }
+            }
+
+            if (videoContentType != MovieType.Mp4)
+            {
+                _VideoMSS = FFmpegInteropMSS.CreateFFmpegInteropMSSFromStream(tempStream, false, false);
+                var mss = _VideoMSS.GetMediaStreamSource();
+                mss.SetBufferedRange(TimeSpan.Zero, TimeSpan.Zero);
+                return MediaSource.CreateFromMediaStreamSource(mss);
+            }
+            else
+            {
+                tempStream.Dispose();
+                tempStream = null;
+
+                return MediaSource.CreateFromUri(videoUri);
+            }
         }
+
+
+        protected override void OnStopStreaming()
+        {
+            _VideoMSS?.Dispose();
+
+            base.OnStopStreaming();
+        }
+
     }
 }
