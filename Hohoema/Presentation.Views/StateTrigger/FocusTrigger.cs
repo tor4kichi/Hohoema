0fbb5d14fef107c00555e40123921a6db049e817
tor4kichi <tor4kichi@gmail.com>
2022-08-25 15:35:14 +0900

    動画を複数回再生した時にCommentTextBoxに対するFocusTriggerが機能しなくなっていた問題を解消。

--- Hohoema/Presentation.Views/StateTrigger/FocusTrigger.cs
+++ Hohoema/Presentation.Views/StateTrigger/FocusTrigger.cs
@@ -5,23 +5,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.Xaml;
+using Microsoft.Toolkit.Uwp.Helpers;
 
 namespace Hohoema.Presentation.Views.StateTrigger
 {
     public class FocusTrigger : UIIntractionTriggerBase
     {
+        private WeakEventListener<UIElement, object, RoutedEventArgs> _gotFocus_we;
+        private WeakEventListener<UIElement, object, RoutedEventArgs> _lostFocus_we;
+
         protected override void OnAttachTargetHandler(UIElement ui)
         {
-            ui.GotFocus -= Ui_GotFocus;
-            ui.LostFocus -= Ui_LostFocus;
-            ui.GotFocus += Ui_GotFocus;
-            ui.LostFocus += Ui_LostFocus;
+            _gotFocus_we = new WeakEventListener<UIElement, object, RoutedEventArgs>(ui)
+            {
+                OnEventAction = (inst, s, e) => Ui_GotFocus(s, e)
+            };
+
+            _lostFocus_we = new WeakEventListener<UIElement, object, RoutedEventArgs>(ui)
+            {
+                OnEventAction = (inst, s, e) => Ui_LostFocus(s, e)
+            };
+
+            ui.GotFocus += _gotFocus_we.OnEvent;
+            ui.LostFocus += _lostFocus_we.OnEvent;
         }
 
         protected override void OnDetachTargetHandler(UIElement ui)
         {
-            ui.GotFocus -= Ui_GotFocus;
-            ui.LostFocus -= Ui_LostFocus;
+            _gotFocus_we.Detach();
+            _lostFocus_we.Detach();
         }
 
 
