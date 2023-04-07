c0457152cd2ba2043f7e796731c12afca4336360
tor4kichi <tor4kichi@gmail.com>
2023-04-08 08:20:55 +0900

    Viewの名前空間内のHohoema/Niconicoを整理。

---
+++ Hohoema/Views.Niconico/User/UserInfoPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Hohoema.ViewModels.Pages.Niconico.VideoRanking;
+using CommunityToolkit.Mvvm.DependencyInjection;
+using Hohoema.ViewModels.Pages.Niconico.User;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
+
+namespace Hohoema.Views.Pages.Niconico.User
+{
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+	public sealed partial class UserInfoPage : Page
+	{
+		public UserInfoPage()
+		{
+			this.InitializeComponent();
+
+			DataContext = _vm = Ioc.Default.GetRequiredService<UserInfoPageViewModel>();
+		}
+
+		private readonly UserInfoPageViewModel _vm;
+	}
+}
