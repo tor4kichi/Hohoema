0e249b69334c019c6051c9ad454cf3acecf016a3
tor4kichi <tor4kichi@gmail.com>
2023-05-27 14:40:54 +0900

    ニコレポページと動画情報ページにおいてダブルクリックによる動画再生が機能していないバグを修正。

--- Hohoema/Views.Niconico/NicoRepo/NicoRepoPage.xaml.cs
+++ Hohoema/Views.Niconico/NicoRepo/NicoRepoPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿#nullable enable
 using CommunityToolkit.Mvvm.DependencyInjection;
+using Hohoema.Models.Application;
+using Hohoema.Services;
 using Hohoema.ViewModels.Pages.Niconico.NicoRepo;
 using System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=234238 を参照してください
 
@@ -38,9 +41,53 @@ public sealed partial class NicoRepoPage : Page
     {
         this.InitializeComponent();
         DataContext = _vm = Ioc.Default.GetRequiredService<NicoRepoPageViewModel>();
+
+        _layoutManager = Ioc.Default.GetRequiredService<ApplicationLayoutManager>();
+        _appearanceSettings = Ioc.Default.GetRequiredService<AppearanceSettings>();
+        Loaded += NicoRepoPage_Loaded;
+    }
+
+    private void NicoRepoPage_Loaded(object sender, RoutedEventArgs e)
+    {
+        if ((_layoutManager.IsMouseInteractionDefault || _layoutManager.IsTouchInteractionDefault)
+             && _appearanceSettings.IsVideoListItemDoubleClickOrDoubleTapToPlayEnabled
+             )
+        {
+            // センタークリック操作のためにItemClickは有効化しておきたい
+            ItemsList.IsItemClickEnabled = true;
+            ItemsList.IsDoubleTapEnabled = true;
+            ItemsList.DoubleTapped += ItemsList_DoubleTapped;
+            ItemsList.ItemClick -= ItemsList_ItemClick;
+        }
+        else
+        {
+            ItemsList.IsItemClickEnabled = true;
+            ItemsList.IsDoubleTapEnabled = false;
+            ItemsList.ItemClick += ItemsList_ItemClick;
+        }
+    }
+
+    private void ItemsList_DoubleTapped(object sender, DoubleTappedRoutedEventArgs e)
+    {
+        if (e.OriginalSource is FrameworkElement fe
+            && _vm.OpenNicoRepoItemCommand.CanExecute(fe.DataContext))
+        {
+            _vm.OpenNicoRepoItemCommand.Execute(fe.DataContext);
+            e.Handled = true;
+        }
+    }
+
+    private void ItemsList_ItemClick(object sender, ItemClickEventArgs e)
+    {
+        if (_vm.OpenNicoRepoItemCommand.CanExecute(e.ClickedItem))
+        {
+            _vm.OpenNicoRepoItemCommand.Execute(e.ClickedItem);
+        }
     }
 
     private readonly NicoRepoPageViewModel _vm;
+    private readonly ApplicationLayoutManager _layoutManager;
+    private readonly AppearanceSettings _appearanceSettings;
 }
 
 
